2ba9d29ca471ac2a06b140174876e606af17755a
tsurusekazuki <b1714935@planet.kanazawa-it.ac.jp>
2020-05-22 17:34:52 +0900

    Improve: create_channel_id_csv

---
+++ src/code/create_channel_id_csv.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import codecs
+from module.create_log.get_logger import get_logger
+from module.youtube_details.get_channel_id import get_channel_id
+
+
+def main():
+    with codecs.open("../data/input/JPvideos.csv", "r", "utf-8", "ignore") as file:
+        df = pd.read_table(file, delimiter=",")
+        video_id = list(df["video_id"])
+        receive_channel_id = pass_channel_id(video_id)
+        write_channel_id_to_csv(receive_channel_id)
+
+
+def pass_channel_id(video_id):
+    push_channel_id = {}
+
+    for idx, vi in enumerate(video_id[120:150]):
+        channel_id = get_channel_id(vi)
+        push_channel_id[vi] = channel_id
+        print(idx, "get: " + channel_id)
+
+    get_logger().info("DONE: GET Channel ID ")
+
+    return push_channel_id
+
+
+def write_channel_id_to_csv(rci):
+    video_id, channel_id = list(rci.keys()), list(rci.values())
+    df = pd.DataFrame({"video_id": video_id, "channel_id": channel_id}, index=None)
+    df.to_csv("../data/input/channel_id.csv", mode="a", header=False)
+
+    get_logger().info("DONE: write to csv")
+
+
+if __name__ == '__main__':
+    main()
+
+# df.to_csv('../data/input/channel_id.csv', mode='a', header=False)
---
+++ src/code/create_subscriber_csv.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_csv("../data/input/channel_id.csv")
+
+print(df.info())
+
